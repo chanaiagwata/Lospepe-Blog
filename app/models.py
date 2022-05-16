59d17c8ed687fe5a66627cbc36d3d5ad5c080383
chanaiagwata <chanai.agwata@student.moringaschool.com>
2022-05-16 19:49:55 +0300

    feat:will add user authentication

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 from datetime import datetime
-from . import db
+
 
 
 
@@ -12,9 +13,20 @@ class User(db.Model):
     last_name = db.Column(db.String(255))
     username = db.Column(db.String(255))
     email = db.Column(db.String(255),unique=True, index=True)
+    pass_secure = db.Column(db.String(255))
     comment = db.relationship("Comments", backref = "user", lazy = "dynamic")
     blogs = db.relationship("Blogs", backref = "user", lazy = "dynamic" )
 
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+    @password.setter
+    def password(self,password):
+        self.pass_secure  = generate_password_hash(password)
+        
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+    
     def __ref__(self):
         return f'User{self.username}'
 #Blogs
