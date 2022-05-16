bac73f7d1396a5a7389ed043e47d9411cc764f4b
chanaiagwata <chanai.agwata@student.moringaschool.com>
2022-05-16 19:30:30 +0300

    feat:will enable database migrations

--- manage.py
+++ manage.py
@@ -1,12 +1,20 @@
 from app import create_app,db
 from flask_script import Manager,Server
 from app.models import User, Blogs, Comment, Quotes, Subscribers
+from flask_migrate import Migrate, MigrateCommand
+
+
+
 
 #Creating app instance
 app = create_app('development')
-
+  
 manager = Manager(app)
 manager.add_command('server',Server)
+
+#Initialize Migrate class
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 #Create a shell context
 @manager.shell
